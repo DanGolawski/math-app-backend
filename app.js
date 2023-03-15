705d47c843c124e32fe93648299fc6c8eb84c3bd
User <user@pc-1888.home>
2023-03-15 09:12:49 +0100

    backend initialization

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const sqlite3 = require('sqlite3').verbose();
+
+const app = express()
+const port = 3000
+
+app.use(express.json({expended: true}))
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+app.get('/videos', (req, res) => {
+  let db = new sqlite3.Database('mathapp.db');
+
+  db.all('SELECT * FROM videos', [], (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    res.send(rows)
+  });
+
+  db.close();
+});
+
+app.post('/videos', (req, res) => {
+  const db = new sqlite3.Database('mathapp.db');
+  
+  db.run(`INSERT INTO videos (author, title, url) VALUES ("${req.body.author}", "${req.body.title}", "${req.body.url}");`, () => {
+    res.send('done')
+  });
+
+  db.close();
+});
